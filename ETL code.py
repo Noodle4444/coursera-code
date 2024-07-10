3c6277e25c58ed6acaecdbcd23458dac11d167fa
Noodle4444 <ianj67@comcast.net>
2024-07-10 14:18:06 -0600

    Update ETL code.py

--- ETL code.py
+++ ETL code.py
@@ -92,3 +92,124 @@ log_progress("Load phase Ended")
   
 # Log the completion of the ETL process 
 log_progress("ETL Job Ended") 
+
+
+
+#For the practice problem 
+
+
+
+import glob 
+import pandas as pd 
+import xml.etree.ElementTree as ET 
+from datetime import datetime
+import zipfile
+import os
+
+log_file = "log_file.txt" 
+target_file = "transformed_data.csv" 
+extracted_folder = "extracted_data"
+
+def extract_from_csv(csv_file):
+    dataframe=pd.read_csv(csv_file)
+    return dataframe
+
+def extract_from_json(json_file):
+    dataframe = pd.DataFrame(columns=['car_model', 'year_of_manufacture', 'price', 'fuel'])
+
+    with open(json_file, 'r') as file:
+        for line in file:
+            try:
+                json_data = pd.read_json(line)
+                if not json_data.empty:
+                    dataframe = pd.concat([dataframe, json_data], ignore_index=True)
+            except ValueError:
+                print(f"Ignoring invalid JSON in file: {json_file}")
+
+    return dataframe
+
+def extract_from_xml(xml_file):
+    dataframe=pd.DataFrame(columns=['car_model', 'year_of_manufacture', 'price', 'fuel'])
+    tree=ET.parse(xml_file)
+    root=tree.getroot()
+    for car in root:
+        car_model= car.find('car_model').text
+        year_of_manufacture= int(car.find('year_of_manufacture').text)
+        price= float(car.find('price').text)
+        fuel=car.find('fuel').text
+        dataframe = pd.concat([dataframe, pd.DataFrame([{"car_model": car_model, "year_of_manufacture": year_of_manufacture, "price": price, "fuel": fuel}])], ignore_index = True)
+    return dataframe
+
+if not os.path.exists(extracted_folder):
+    os.makedirs(extracted_folder)
+
+# Extract files from all ZIP files in the folder
+zip_files = glob.glob("*.zip")
+for zip_file in zip_files:
+    with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+        zip_ref.extractall(extracted_folder)
+
+#here i detect type of file
+def extract():
+    extracted_data = pd.DataFrame(columns=['car_model', 'year_of_manufacture', 'price', 'fuel'])
+
+    for csvfile in glob.glob(os.path.join(extracted_folder, "*.csv")):
+        df = pd.DataFrame(extract_from_csv(csvfile))
+        if not df.empty:
+            extracted_data = pd.concat([extracted_data, df], ignore_index=True)
+
+    for jsonfile in glob.glob(os.path.join(extracted_folder, "*.json")):
+        df = pd.DataFrame(extract_from_json(jsonfile))
+        if not df.empty:
+            extracted_data = pd.concat([extracted_data, df], ignore_index=True)
+
+    for xmlfile in glob.glob(os.path.join(extracted_folder, "*.xml")):
+        df = pd.DataFrame(extract_from_xml(xmlfile))
+        if not df.empty:
+            extracted_data = pd.concat([extracted_data, df], ignore_index=True)
+
+    return extracted_data
+
+def transform(data):
+    data['price']=round(data.price, 2)
+    return data
+
+def load_data(target_file, transformed_data):
+    transformed_data.to_csv(target_file, index=False)
+
+def log_progress(message):
+    timestamp_format = '%Y-%h-%d-%H:%M:%S' # Year-Monthname-Day-Hour-Minute-Second 
+    now = datetime.now() # get current timestamp 
+    timestamp = now.strftime(timestamp_format) 
+    with open(log_file,"a") as f: 
+        f.write(timestamp + ',' + message + '\n') 
+
+
+# Log the initialization of the ETL process 
+log_progress("ETL Job Started") 
+ 
+# Log the beginning of the Extraction process 
+log_progress("Extract phase Started") 
+extracted_data = extract() 
+ 
+# Log the completion of the Extraction process 
+log_progress("Extract phase Ended") 
+ 
+# Log the beginning of the Transformation process 
+log_progress("Transform phase Started") 
+transformed_data = transform(extracted_data) 
+print("Transformed Data") 
+print(transformed_data) 
+ 
+# Log the completion of the Transformation process 
+log_progress("Transform phase Ended") 
+ 
+# Log the beginning of the Loading process 
+log_progress("Load phase Started") 
+load_data(target_file,transformed_data) 
+ 
+# Log the completion of the Loading process 
+log_progress("Load phase Ended") 
+ 
+# Log the completion of the ETL process 
+log_progress("ETL Job Ended") 
